9c2cca7bd2d902b6736390b04747bbb556f3ee4f
loan-jjyu <user@loan-jjyu.local>
2026-03-08 13:29:55 +0900

    feat: sitemap + robots.txt 추가

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://loan.jjyu.co.kr/sitemap.xml",
+    host: "https://loan.jjyu.co.kr",
+  };
+}
